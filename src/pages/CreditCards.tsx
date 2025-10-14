3434978140476aff385a5704c5c24b5f63146ef0
[dyad] <edderdias@gmail.com>
2025-10-14 17:20:36 -0300

    Updated src/pages/CreditCards.tsx

--- src/pages/CreditCards.tsx
+++ src/pages/CreditCards.tsx
@@ -18,7 +18,7 @@ const formatCurrencyDisplay = (value: number | undefined): string => {
   return new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
-    minimumFractionDigits: 10,
+    minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(value);
 };
